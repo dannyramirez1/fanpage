8ea96f185e216c03f7cffb8047a085957dc58f97
dannyramirez1 <37849344+dannyramirez1@users.noreply.github.com>
2020-05-20 16:16:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -27,10 +27,38 @@ function videoThree(){
 	g.classList.add("appearance");
 	h.classList.remove("appearance");
 }
-
+function resetImages(){
+	function undoFour(){
+	}
+	function undoOne(){
+	}
+	function undoTwo(){
+	}
+	function undoThree(){
+	}
+	b.pause("appearance");
+	d.pause("appearance");
+	f.pause("appearance");
+	h.pause("appearance");
+}
+function undoFour(){
+	a.classList.remove("appearance");
+	b.classList.add("appearance");
+}
+function undoOne(){
+	c.classList.remove("appearance");
+	d.classList.add("appearance");
+}
+function undoTwo(){
+	e.classList.remove("appearance");
+	f.classList.add("appearance");
+}
+function undoThree(){
+	g.classList.remove("appearance");
+	h.classList.add("appearance");
+}
 function nextImage(){
 	if (i.src.match("images/trippie.jpg")){
-		function resetImages(){
 		 i.src = "images/sob.jpg";
 		 a.src = "images/hitboy.jpg";
 		 c.src = "images/rbe.jpg";
@@ -42,9 +70,7 @@ function nextImage(){
 		 b.src = "videos/family.mp4";
 		 k.innerHTML = "SOBxRBE" 
 		 l.innerHTML = "SOB x RBE consists of Yhung T.O., DaBoii, Slimmy B, and Lul G. SOB x RBE, often called simply SOB, originate from Vallejo, CA, and have been mentored by rapper Sage the Gemini. The groups name stands for Strictly Only Brothers. Slimmy B, DaBoii, and Lul G were all part of SOB. RBE stands for Real Boy Entertainment and was Yhung T.O.’s groups. The 2 groups combined into SOB x RBE when they were in high school and started recording music together." 
-		}
 	} else if ( i.src.match("images/sob.jpg")){
-		function resetImages(){
 		 i.src = "images/dsavage.jpg";
 		 a.src = "images/know.jpg";
 		 c.src = "images/dphoenix.jpg";
@@ -56,9 +82,7 @@ function nextImage(){
 		 b.src = "videos/iknow.mp4";
 		 k.innerHTML = "D.Savage" 
 		 l.innerHTML = "Fresh out of high school from Gardena, California, D Savage is already showing promise in his skills as a young rapper. With close ties to Instagram icons John Ross and Ian Connor, he also has close ties to fellow rappers such as Tyler, The Creator, A$AP Rocky and many more." 
-		}
 	} else if ( i.src.match("images/dsavage.jpg")){
-		function resetImages(){
 		 i.src = "images/trippie.jpg";
 		 a.src = "images/mark.jpg";
 		 c.src = "images/loveletter.jpg";
@@ -70,22 +94,7 @@ function nextImage(){
 		 b.src = "videos/snakeskin.mp4";
 		 k.innerHTML = "Trippie Redd" 
 		 l.innerHTML = "Hailing from Canton, Ohio, Trippie Redd, real name Michael White IV (born June 18, 1999), who is well known for his singles, “Love Scars/You hurt me” and “Romeo and Juliet”. Influenced by Lil Wayne, A$AP Rocky, T-Pain, and even popular rock band KISS. In the interview, Trippie explains that the reason he began rapping to begin with was to honor his older brother, who he looked up to as a role model and had passed away while Trippie was still young. Trippie’s original name was Hippie Redd. He had then changed it to Trippy Hippie, and chose to combine the two thus forming the name Trippie Redd. So far, Redd has released five mixtapes and collaborated with the likes of Kodie Shane, 6ix9ine, XXXTENTACION, Famous Dex, and Uno The Activist."
-		}
 	}else {
 		// do nothing
 	}
-function resetImages(){
-	b.classList.add("appearance");
-	d.classList.add("appearance");
-	f.classList.add("appearance");
-	h.classList.add("appearance");
-	b.pause("appearance");
-	d.pause("appearance");
-	f.pause("appearance");
-	h.pause("appearance");
-	a.classList.remove("appearance");
-	c.classList.remove("appearance");
-	e.classList.remove("appearance");
-	g.classList.remove("appearance");
-}
 }
